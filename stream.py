77fb1b8f1fce3d4de827c14fe55d770e98fd68c8
Aoto Sasaki <aoto.sasaki@eitoss.com>
2024-01-18 18:17:21 +0900

    add pkl_files

--- stream.py
+++ stream.py
@@ -7,8 +7,8 @@ import streamlit as st
 
 # 変数の定義
 path = 'https://raw.githubusercontent.com/ivy-consulting/iwane-demo/main/components/pkl_files'
-pkl_tot = 'components/pkl_files/tot_iwane.pkl'
-pkl_vec = 'components/pkl_files/vec_iwane.pkl'
+# pkl_tot = 'components/pkl_files/tot_iwane.pkl'
+# pkl_vec = 'components/pkl_files/vec_iwane.pkl'
 pkl_tots = [f'{path}/tot_iwane1.pkl', f'{path}/tot_iwane2.pkl', f'{path}/tot_iwane3.pkl', f'{path}/tot_iwane4.pkl', f'{path}/tot_iwane5.pkl', f'{path}/tot_iwane6.pkl']
 pkl_vecs = [f'{path}/vec_iwane1.pkl', f'{path}/vec_iwane2.pkl', f'{path}/vec_iwane3.pkl', f'{path}/vec_iwane4.pkl', f'{path}/vec_iwane5.pkl', f'{path}/vec_iwane6.pkl']
 table_name = 'iwane'
